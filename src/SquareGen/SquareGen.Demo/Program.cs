410f0e480db4586e06c21f6e2fe2821044644827
0xFireball <0xFireball@outlook.com>
2017-10-06 13:02:23 -0700

    create project

---
+++ src/SquareGen/SquareGen.Demo/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SquareGen.Demo
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+        }
+    }
+}
