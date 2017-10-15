91413cba0572e0dc8516d5efec28eec33f96c48a
0xFireball <0xFireball@outlook.com>
2017-10-14 20:37:49 -0700

    add Nez project

---
+++ src/SquareGen/SquareGen.RenderDemo/Game1.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Nez;
+using SquareGen.RenderDemo.Scenes;
+
+namespace SquareGen.RenderDemo
+{
+    /// <summary>
+    /// This is the main type for your game.
+    /// </summary>
+    public class Game1 : Core
+    {
+        public Game1() : base(1280, 720, false, true, "RenderDemo")
+        {
+        }
+
+        /// <summary>
+        /// Allows the game to perform any initialization it needs to before starting to run.
+        /// This is where it can query for any required services and load any non-graphic
+        /// related content.  Calling base.Initialize will enumerate through any components
+        /// and initialize them as well.
+        /// </summary>
+        protected override void Initialize()
+        {
+            base.Initialize();
+
+            if (System.Environment.OSVersion.Platform == System.PlatformID.Win32NT)
+            {
+                Window.Position = new Point(200, 200);
+            }
+
+            scene = new MainScene();
+        }
+    }
+}
---
+++ src/SquareGen/SquareGen.RenderDemo/Scenes/MainScene.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.Xna.Framework;
+using Nez;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SquareGen.RenderDemo.Scenes
+{
+    class MainScene : Scene
+    {
+        private DefaultRenderer renderer;
+
+        public MainScene()
+        {
+            renderer = addRenderer(new DefaultRenderer());
+            clearColor = Color.GhostWhite;
+        }
+
+        public override void initialize()
+        {
+            base.initialize();
+
+            var box = createEntity("box", new Vector2(400, 400));
+            box.addComponent(new Cube3D());
+        }
+
+        public override void update()
+        {
+            base.update();
+        }
+    }
+}
